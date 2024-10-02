03c34b6db76e863304d4de2bc211ee1202449dc5
Siti Usnul Khotimah <34421708+cangka22@users.noreply.github.com>
2024-10-02 10:18:32 +0700

    Update asahotak.ts

--- packages/scraper-games/src/asahotak.ts
+++ packages/scraper-games/src/asahotak.ts
@@ -5,7 +5,7 @@ export let asahotakjson: AsahOtak[]
 export default async function asahotak (): Promise<AsahOtak> {
   if (!asahotakjson) {
     asahotakjson = await got(
-      'https://raw.githubusercontent.com/BochilTeam/database/master/games/asahotak.json'
+      'https://raw.githubusercontent.com/cangka22/database/master/games/asahotak.json'
     ).json()
   }
   return AsahOtakSchema.parse(
