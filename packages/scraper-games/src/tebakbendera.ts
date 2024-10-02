f55a5d7242603acc796995978207ec135d666653
Siti Usnul Khotimah <34421708+cangka22@users.noreply.github.com>
2024-10-02 10:22:37 +0700

    Update tebakbendera.ts

--- packages/scraper-games/src/tebakbendera.ts
+++ packages/scraper-games/src/tebakbendera.ts
@@ -5,7 +5,7 @@ export let tebakbenderajson: TebakBendera[]
 export default async function tebakbendera (): Promise<TebakBendera> {
   if (!tebakbenderajson) {
     tebakbenderajson = await got(
-      'https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakbendera.json'
+      'https://raw.githubusercontent.com/cangka22/database/master/games/tebakbendera.json'
     ).json()
   }
   return TebakBenderaSchema.parse(
